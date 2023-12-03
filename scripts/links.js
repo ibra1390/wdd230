5ca219434af6a299617a45e02e251883c7f6964b
ibra1390 <ing.gonzalezvij@hotmail.com>
2023-12-03 12:47:00 -0600

    links json updated

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,28 @@
 const baseURL="https://ibra1390.github.io/wdd230/";
 const linksURL = "https://ibra1390.github.io/wdd230/data/links.json";
+let ul = document.getElementById('activities');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
-}
+    displayLinks(data.weeks);
+  }
 
-
+  function displayLinks(weeks){
+    weeks.forEach((week) => {
+        let li = document.createElement('li');
+        li.textContent = `${week["week"]}: `;
+        
+        aElements = week["links"];
+        aElements.forEach((element)=>{
+            let a = document.createElement('a');
+            a.textContent = ` ${element["title"]} |`;
+            a.href = `${element["url"]}`;
+            li.appendChild(a);
+        });
+    
+        ul.appendChild(li);
+    });
+  }
 
 getLinks();
